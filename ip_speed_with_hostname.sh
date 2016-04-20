098d70016510e2b1cd07f4a98f361187e512837c
yech1990 <rock5719633@gmail.com>
2016-04-20 17:04:33 +0800

    update

--- ip_speed_with_hostname.sh
+++ ip_speed_with_hostname.sh
@@ -11,9 +11,11 @@ sleep 1
 echo "Active Downloading Terminals:"
 echo ""
 iptables -nvx -L FORWARD | grep DOWNLOAD | awk 'BEGIN{OFS="\t"}NR==FNR{a[$3]=$4}NR>FNR{if($2>0)print $2/1024/1" KB/s ",$1/10" packets/s", a[$9], $9}' /tmp/dhcp.leases - | sort -n -r
-echo "" echo "Active Uploading Terminals:"
+echo "Active Uploading Terminals:"
 echo ""
 iptables -nvx -L FORWARD | grep UPLOAD | awk 'BEGIN{OFS="\t"}NR==FNR{a[$3]=$4}NR>FNR{if($2>0)print $2/1024/1" KB/s ",$1/10" packets/s", a[$8], $8}' /tmp/dhcp.leases - | sort -n -r
 
 while read line;do iptables -D FORWARD -s $line -j UPLOAD;done < mac-ip
 while read line;do iptables -D FORWARD -d $line -j DOWNLOAD;done < mac-ip
+iptables -X UPLOAD
+iptables -X DOWNLOAD
