d5f61f9c5f360d37b166e610a0cb4c1db31e7a44
yech1990 <rock5719633@gmail.com>
2016-04-20 16:53:21 +0800

    update

--- ip_speed_with_hostname.sh
+++ ip_speed_with_hostname.sh
@@ -13,8 +13,7 @@ sleep 1
 echo "Active Downloading Terminals:"
 echo ""
 iptables -nvx -L FORWARD | grep DOWNLOAD | awk 'BEGIN{OFS="\t"}NR==FNR{a[$3]=$4}NR>FNR{if($2>0)print $2/1024/1" KB/s ",$1/10" packets/s", a[$9], $9}' /tmp/dhcp.leases - | sort -n -r
-echo ""
-echo "Active Uploading Terminals:"
+echo "" echo "Active Uploading Terminals:"
 echo ""
 iptables -nvx -L FORWARD | grep UPLOAD | awk 'BEGIN{OFS="\t"}NR==FNR{a[$3]=$4}NR>FNR{if($2>0)print $2/1024/1" KB/s ",$1/10" packets/s", a[$8], $8}' /tmp/dhcp.leases - | sort -n -r
 
